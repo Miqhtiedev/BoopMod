012775d58d0df332b4969691692de708e8fd0156
RegexMC <56909681+RegexMC@users.noreply.github.com>
2020-12-20 21:04:19 +1100

    Made code not as bad
    moved everything to one command, added tab completion, reformatted code, added myself to credits :), only tested the boopback

--- src/main/java/me/miqhtie/boopmod/multithreading/Multithreading.java
+++ src/main/java/me/miqhtie/boopmod/multithreading/Multithreading.java
@@ -1,25 +1,25 @@
 package me.miqhtie.boopmod.multithreading;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Multithreading {
-    private HashMap<String, Long> chatQueue = new HashMap<String, Long>();
+    private final HashMap<String, Long> chatQueue = new HashMap<>();
     private ChatThread chatThread = null;
-    public void queueMessage(String message, Long delay){
+
+    public void queueMessage(String message, Long delay) {
         chatQueue.put(message, delay);
-        if(chatThread == null){
+        if (chatThread == null) {
             System.out.println("No chat thread exists, creating new one.");
             chatThread = new ChatThread();
             chatThread.start();
         }
     }
 
-    public void setChatThread(ChatThread thread){
+    public void setChatThread(ChatThread thread) {
         chatThread = thread;
     }
 
-    public HashMap<String, Long> getChatQueue(){
+    public HashMap<String, Long> getChatQueue() {
         return chatQueue;
     }
 }
--- src/main/java/me/miqhtie/boopmod/utils/WhitelistConfig.java
+++ src/main/java/me/miqhtie/boopmod/utils/WhitelistConfig.java
@@ -1,6 +1,10 @@
 package me.miqhtie.boopmod.utils;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class WhitelistConfig {
